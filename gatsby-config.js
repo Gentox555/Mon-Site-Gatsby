01e2a40a593f5981a669a7dace8b2edd51cdd7c4
Boudraa <anasanas99b@gmail.com>
2023-10-28 17:30:37 +0200

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "Mon Site Gatsby",
+    title: "My Super Cool Blog",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
-      }
+        path: `${__dirname}/blog/`,
+      },
     },
+    "gatsby-plugin-mdx",
   ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,33 +4,35 @@ import Layout from '../components/layout'
 import Seo from '../components/seo'
 
 const BlogPage = ({ data }) => {
-    console.log(data);
   return (
     <Layout pageTitle="My Blog Posts">
-    
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
-      </ul>
-
     </Layout>
-  );
-};
+  )
+}
 
 export const query = graphql`
-query {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
-    nodes {
-      name
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        excerpt
+      }
     }
   }
-}
-`;
+`
 
 export const Head = () => <Seo title="My Blog Posts" />
 
